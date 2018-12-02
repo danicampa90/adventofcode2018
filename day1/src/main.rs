99eec44719d2c5d7a7cdb12543d807eec8819f84
dani campa <danicampa90@gmail.com>
2018-12-02 19:01:56 +0100

    Day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,66 @@
+extern crate libutils;
+use libutils::read_file_lines_in_chan;
+use std::collections::HashSet;
+use std::thread;
+use std::sync::mpsc::channel;
+
+fn second_part(numbers: Vec<i32>) -> i32 {
+    
+    let mut seen_twice_dict = HashSet::new();
+    let mut sum = 0;
+    for number in numbers.into_iter().cycle() {
+        
+        if !seen_twice_dict.insert(sum) {
+            break;
+        }
+        sum += number;
+    }
+    return sum;
+}
+
+
+
 fn main() {
     println!("Hello, world!");
+    let file_rx = read_file_lines_in_chan("input.txt").expect("Cannot open the file");
+
+    let mut sum:i32 = 0;
+
+    let mut numbers : Vec<i32> = Vec::new();
+
+    loop {
+        match file_rx.recv() {
+            Ok(r) => {
+                let number = r.parse::<i32>().expect("Not a number!");
+                numbers.push(number);
+                sum+= number;
+            },
+            Err(_) => break
+        }
+    }
+    println!("Resulting frequency: {}", sum);
+    let repeats_at = second_part(numbers);
+    println!("It repeats at value {}", repeats_at);
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::second_part;
+    #[test]
+    fn second_start_test_1() {
+        assert_eq!(second_part(vec!(1, -1)), 0);
+    }
+    #[test]
+    fn second_start_test_2() {
+        assert_eq!(second_part(vec!(3, 3, 4, -2, -4)), 10);
+        }
+    #[test]
+    fn second_start_test_3() {
+        assert_eq!(second_part(vec!(-6, 3, 8, 5, -6)), 5);
+        }
+    #[test]
+    fn second_start_test_4() {
+        assert_eq!(second_part(vec!(7, 7, -2, -7, -4)), 14);
+    }
 }
--- libutils/src/lib.rs
+++ libutils/src/lib.rs
@@ -1,3 +1,13 @@
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::sync::mpsc::channel;
+use std::sync::mpsc::Receiver;
+use std::thread;
+
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -5,3 +15,56 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+pub fn read_file_to_str(fname: &str) -> Result<String, std::io::Error> {
+    println!("Loading {0}", fname);
+    let mut file : File = File::open(Path::new(fname))?;
+    let mut s = String::new();
+    let size = file.read_to_string(&mut s)?;
+    println!("Read {} bytes",size );
+    return Ok(s)
+}
+
+#[derive(Debug)]
+pub enum FileProcessingErr<E>{
+    IoError(std::io::Error),
+    ProcessingError(E)
+}
+
+impl<E> std::convert::From<std::io::Error> for FileProcessingErr<E> {
+    fn from(err: std::io::Error) -> Self {
+        FileProcessingErr::IoError(err)
+    }
+}
+
+pub fn read_file_foreach_line<F, E>(fname: &str, action: &mut F) -> Result<(), FileProcessingErr<E>>
+    where F: FnMut(String)-> Result<(), E> 
+{
+    let f = try!(File::open(fname));
+    let file = BufReader::new(&f);
+    for line in file.lines() {
+        let l = line.unwrap();
+        if let Err(res) = action(l)
+        {
+            return Err(FileProcessingErr::ProcessingError::<E>(res));
+        }
+        
+    }
+    Ok(())
+}
+
+pub fn read_file_lines_in_chan(fname: &str) -> Result<Receiver<String>, std::io::Error>
+{
+    let f = try!(File::open(fname));
+    let (sender, recv) = channel();
+    thread::spawn(move || {
+        let file = BufReader::new(&f);
+        
+        for line in file.lines() {
+            let l = line.unwrap();
+            if sender.send(l).is_err() {return;};
+        } 
+    });
+    Ok(recv)
+}
+
